1584fa21d869b0a381ad43a6e87f3a94937459d3
Gustavo Takahashi <gustavokenjitakahashi@gmail.com>
2023-02-01 00:15:05 -0300

    finish project

--- src/App.js
+++ src/App.js
@@ -10,21 +10,29 @@ function App() {
   const [topScore, setTopScore] = useState(0);
   const [attempts, setAttempts] = useState([]);
   const images = Images();
+  const numbers = [];
 
   const checkCard = (e) => {
-    if (e) {
-      setAttempts([...attempts, "teste"]);
-      console.log(attempts);
-      return setScore(currentScore + 1);
-    }
-    alert("Game over!");
-    return updateTopScore();
+    const description = e.target.nextElementSibling.innerHTML;
+    attempts.includes(description) ? endGame() : continueGame(description);
   };
 
-  const updateTopScore = () => {
+  const continueGame = (description) => {
+    setAttempts([...attempts, description]);
+    setScore(currentScore + 1);
+  };
+
+  const endGame = () => {
+    alert("ERRROUUUUUUUU");
     topScore < currentScore ? setTopScore(currentScore) : setTopScore(topScore);
+    setAttempts([]);
+    setScore(0);
   };
 
+  const generateRandomNumber = () => {
+    const limit = images.length - 1;
+    return Math.floor(Math.random() * (limit + 1));
+  };
   useEffect(() => {
     document.title = "Score: " + currentScore;
   });
@@ -34,7 +42,18 @@ function App() {
       <Score currentScore={currentScore} topScore={topScore} />
       <div className="cards">
         {images.map((image) => {
-          return <Card src={image.src} description={image.description} />;
+          let random_number = generateRandomNumber();
+          do {
+            random_number = generateRandomNumber();
+          } while (numbers.includes(random_number) == true);
+          numbers.push(random_number);
+          return (
+            <Card
+              src={images[random_number].src}
+              description={images[random_number].description}
+              onClick={checkCard}
+            />
+          );
         })}
       </div>
     </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,7 @@ import "../styles/Card.css";
 export default function Card(props) {
   return (
     <div className="card">
-      <img className="image" src={props.src} />
+      <img className="image" src={props.src} onClick={props.onClick} />
       <p>{props.description}</p>
     </div>
   );
